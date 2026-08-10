7edb22a7e34fc6e15ed4e7f509c75dc26f6a22ed
Rudra32944 <rudrag526@gmail.com>
2026-08-10 13:42:13 +0530

    Add files via upload

--- code2.c
+++ code2.c
@@ -1,22 +1,13 @@
 #include<stdio.h>
 int main(){
-int a;
+int a ;
 printf("enter a number a");
 scanf("%d",&a);
-int b;
+int b ;
 printf("enter a number b");
 scanf("%d",&b);
-int c;
+int c ;
 c = a+b;
-printf("sum of a and b is :%d",c);
-int d;
-d = a-b;
-printf("difference of a and b is :%d",d);
-int e;
-e = a*b;
-printf("product of a and b is :%d",e);
-float f;
-f = a/b;
-printf("qoteint of a and b is :%f",f);
+printf("sum is :%d",c);
 return 0 ;
 }
