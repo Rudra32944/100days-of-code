d0b4df73a61516531756dd126950dad317af8826
Rudra32944 <rudrag526@gmail.com>
2026-08-23 12:22:16 +0530

    Add files via upload

---
+++ code12.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+char main(){
+char c;
+prinf("enter a charater:");
+scanf("%c",&c);
+if(c=="a"||c=="e"||c=="i"||c=="O"||c=="U"){
+printf("%c is a vowel",c);
+}
+else{
+printf("%c is a consonant",c);
+}
+return 0;
+}
