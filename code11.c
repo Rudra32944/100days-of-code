d863e4f54f39ed0d0bd945a905654f422cf0f440
Rudra32944 <rudrag526@gmail.com>
2026-08-23 10:57:36 +0530

    Add files via upload

---
+++ code11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int y;
+if(y%4==0){
+printf("y is a leap year");
+}
+else if(y%100==0){
+printf("y is a leap year");
+}
+else(y%400==0){
+printf("y is not a leap year");
+}
+return 0;
