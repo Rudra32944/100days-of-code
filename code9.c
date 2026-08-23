41951eb23217978b4d7ae4811184f850ccdd5ffd
Rudra32944 <rudrag526@gmail.com>
2026-08-23 13:09:00 +0530

    Add files via upload

---
+++ code9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int n;
+printf("enter the number n :");
+scanf("%d",&n);
+if(n%2==0);
+printf("the number %d is even:",n);
+else(n%2!=0);
+printf("the number %d is odd:",n);
+return 0;
+}
