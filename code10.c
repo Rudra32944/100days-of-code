bfa2caa9f78fae763b1b2aebca7f8880d423cfdf
Rudra32944 <rudrag526@gmail.com>
2026-08-23 10:56:51 +0530

    Add files via upload

---
+++ code10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int n;
+if(n>=0){
+if(n>0){
+printf("the number n is positive integer");
+}
+else{
+printf("the number is zero");
+}
+}
+if(n<0){
+printf("the given number is negative");
+}
+return 0;
+}
